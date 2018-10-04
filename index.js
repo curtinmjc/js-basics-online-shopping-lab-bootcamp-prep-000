7b02ea529e60f8cc5288914c40e33a91b401454c
Mike Curtin <curtinmjc@gmail.com>
2018-10-04 17:32:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,25 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ item = {itemName: item, itemPrice: Math.floor(Math.random() * 101)};
+ cart.push(item);
+ return item.itemName + " has been added to your cart."
 }
 
 function viewCart() {
   // write your code here
+  var cartAsString;
+  if (cart.length === 0) {
+    cartString = "Your shopping cart is empty.";
+  } else {
+    cartAsString = "In your cart, you have ";
+    for (var num = 0; num < cart.length - 1; num++) {
+      cartAsString = cartAsString + cart[num].itemName + " at $" + cart[num].itemPrice + ", ";
+    }
+    cartAsString = cartAsString + "and " + cart[num].itemName + " at $" + cart[num].itemPrice + ".";
+  }
+  }
+  return cartAsString;
 }
 
 function total() {
