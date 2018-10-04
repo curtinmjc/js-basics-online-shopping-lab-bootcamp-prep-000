dcacf9f23476033de8e31cf5acd2e149fd105027
Mike Curtin <curtinmjc@gmail.com>
2018-10-04 17:38:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ function viewCart() {
   var cartAsString;
   if (cart.length === 0) {
     cartAsString = "Your shopping cart is empty.";
+  } else if (cart.length === 1) {
+    cartAsString = "In your cart, you have " + cart[0].itemName + " at $" + cart[num].itemPrice + "." ;
   } else {
     cartAsString = "In your cart, you have ";
     for (var num = 0; num < cart.length - 1; num++) {
